5a75ca9e7e138264ddcf91437445725a34c4c5f1
victoranderssen <victor.anderssen@gmail.com>
2020-04-24 22:06:57 +0300

    Save

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,15 +1,18 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import rootReducer from './reducers/reducers'
+import thunk from 'redux-thunk'
 
 const loggerMiddleware = createLogger()
+const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
 
-// export default function configureStore(preloadedState: any) {
 export default function configureStore() {
   return createStore(
     rootReducer,
     // preloadedState,
-    applyMiddleware(thunkMiddleware, loggerMiddleware)
+    composeEnhancers(
+      applyMiddleware(thunk)
+    )
   )
 }
